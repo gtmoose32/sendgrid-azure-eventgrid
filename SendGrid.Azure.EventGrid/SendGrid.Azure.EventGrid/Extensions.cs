070ff0ee360c253e53f40bee03e36c3cff814989
Marty Mathis <Marty.Mathis@assurant.com>
2019-12-14 09:00:23 -0500

    Extensions to convert SendGrid events into EventGrid events

---
+++ SendGrid.Azure.EventGrid/SendGrid.Azure.EventGrid/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Azure.EventGrid.Models;
+using Sendgrid.Webhooks.Events;
+
+namespace SendGrid.Azure.EventGrid
+{
+    public static class Extensions
+    {
+        public static EventGridEvent ToEventGridEvent(this WebhookEventBase @event, string subject)
+        {
+            return new EventGridEvent(
+                Guid.NewGuid().ToString(),
+                subject,
+                @event,
+                @event.EventType.ToString(),
+                DateTime.UtcNow,
+                "1");
+        }
+
+        public static EventGridEvent ToEventGridEvent(this WebhookEventBase @event, Func<WebhookEventBase, string> buildSubject) 
+            => @event.ToEventGridEvent(buildSubject(@event));
+    }
+}
