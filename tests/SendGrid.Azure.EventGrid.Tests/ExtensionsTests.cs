fc962c084418c6198b24176eb101ed0ae642e5c4
Marty Mathis <Marty.Mathis@assurant.com>
2019-12-14 20:34:05 -0500

    Added unit test project

---
+++ tests/SendGrid.Azure.EventGrid.Tests/ExtensionsTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SendGrid.Azure.EventGrid.Tests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
