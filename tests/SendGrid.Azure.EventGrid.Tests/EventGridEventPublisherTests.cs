9ce21dd44b75bb803e60a0fc389ccbc7b4b199b1
Marty Mathis <Marty.Mathis@assurant.com>
2019-12-19 09:28:41 -0500

    Added unit tests for library with full code coverage

---
+++ tests/SendGrid.Azure.EventGrid.Tests/EventGridEventPublisherTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SendGrid.Azure.EventGrid.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class EventGridEventPublisherTests
+    {
+        private const string TopicHostName = "topic.host.com";
+
+        private IEventGridClient _eventGridClient;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _eventGridClient = Substitute.For<IEventGridClient>();
+            
+        }
+
+        [TestMethod]
+        public async Task PublishEventsAsync_Test()
+        {
+            //Arrange 
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "SendGridEvents.json");
+            var json = File.ReadAllText(path);
+
+            var sut = new EventGridEventPublisher(_eventGridClient, TopicHostName);
+
+            //Act
+            await sut.PublishEventsAsync(json).ConfigureAwait(false);
+
+            //Assert
+            await _eventGridClient.Received()
+                .PublishEventsWithHttpMessagesAsync(Arg.Is(TopicHostName), Arg.Is<IList<EventGridEvent>>(events => events.Count == 11))
+                .ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task PublishEventsAsync_WithSubjectBuilder_Test()
+        {
+            //Arrange 
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "SendGridEvents.json");
+            var json = File.ReadAllText(path);
+
+            var sut = new EventGridEventPublisher(_eventGridClient, TopicHostName, e => $"/sendgrid/events/{e.EventType}/{e.SgEventId}");
+
+            //Act
+            await sut.PublishEventsAsync(json).ConfigureAwait(false);
+
+            //Assert
+            await _eventGridClient.Received()
+                .PublishEventsWithHttpMessagesAsync(Arg.Is(TopicHostName), Arg.Is<IList<EventGridEvent>>(events => events.Count == 11))
+                .ConfigureAwait(false);
+        }
+    }
+}
--- tests/SendGrid.Azure.EventGrid.Tests/ExtensionsTests.cs
+++ tests/SendGrid.Azure.EventGrid.Tests/ExtensionsTests.cs
@@ -1,13 +1,59 @@
+using AutoFixture;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sendgrid.Webhooks.Events;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SendGrid.Azure.EventGrid.Tests
 {
+    [ExcludeFromCodeCoverage]
     [TestClass]
     public class ExtensionsTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly IFixture _fixture = new Fixture();
+
+        private WebhookEventBase _sut;
+
+        [TestInitialize]
+        public void Init()
         {
+            _sut = _fixture.Build<BounceEvent>()
+                .With(e => e.EventType, WebhookEventType.Bounce)
+                .Without(be => be.UniqueParameters)
+                .Create();
+        }
+
+        [TestMethod]
+        public void ToEventGridEvent_WithSubject_Test()
+        {
+            //Arrange 
+            const string subject = "test-subject";
+
+            //Act
+            var result = _sut.ToEventGridEvent(subject);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Subject.Should().Be(subject);
+            result.Data.Should().BeOfType<BounceEvent>();
+        }
+
+        [TestMethod]
+        public void ToEventGridEvent_WithSubjectBuilder_Test()
+        {
+            //Arrange 
+            const string correlationKey = "correlation-id";
+            var id = _fixture.Create<string>();
+            _sut.UniqueParameters = new Dictionary<string, string> {{correlationKey, id}};
+            
+            //Act
+            var result = _sut.ToEventGridEvent(e => $"/sendgrid/events/{e.EventType}/{e.UniqueParameters[correlationKey]}");
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Subject.Should().Be($"/sendgrid/events/{WebhookEventType.Bounce}/{_sut.UniqueParameters[correlationKey]}");
+            result.Data.Should().BeOfType<BounceEvent>();
         }
     }
 }
