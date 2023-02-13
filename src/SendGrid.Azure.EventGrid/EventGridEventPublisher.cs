857afe364aacd1d0c75a0f4dc71e15751b69084f
Marty Mathis <Marty.Mathis@assurant.com>
2023-02-13 09:31:55 -0500

    Updated project to use nuget package best practices along with new lang features

--- src/SendGrid.Azure.EventGrid/EventGridEventPublisher.cs
+++ src/SendGrid.Azure.EventGrid/EventGridEventPublisher.cs
@@ -8,56 +8,55 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Moosesoft.SendGrid.Azure.EventGrid
+namespace Moosesoft.SendGrid.Azure.EventGrid;
+
+/// <summary>
+/// Reads SendGrid events json posted to a webhook, converts them to <see cref="EventGridEvent"/> and publishes those events to a topic.
+/// </summary>
+public class EventGridEventPublisher : IEventGridEventPublisher
 {
+    private readonly EventGridEventPublisherSettings _settings;
+    private readonly EventGridPublisherClient _eventGridClient;
+
     /// <summary>
-    /// Reads SendGrid events json posted to a webhook, converts them to <see cref="EventGridEvent"/> and publishes those events to a topic.
+    /// Initialize a new instance of <see cref="EventGridEventPublisher"/>.
     /// </summary>
-    public class EventGridEventPublisher : IEventGridEventPublisher
+    /// <param name="eventGridClient">The client used to publish SendGrid events to an Azure EventGrid topic.</param>
+    /// <param name="settings">Settings used to help build events.</param>
+    public EventGridEventPublisher(EventGridPublisherClient eventGridClient, EventGridEventPublisherSettings settings = null)
     {
-        private readonly EventGridEventPublisherSettings _settings;
-        private readonly EventGridPublisherClient _eventGridClient;
+        _eventGridClient = eventGridClient ?? throw new ArgumentNullException(nameof(eventGridClient));
+        _settings = settings ?? EventGridEventPublisherSettings.Default;
+    }
 
-        /// <summary>
-        /// Initialize a new instance of <see cref="EventGridEventPublisher"/>.
-        /// </summary>
-        /// <param name="eventGridClient">The client used to publish SendGrid events to an Azure EventGrid topic.</param>
-        /// <param name="settings">Settings used to help build events.</param>
-        public EventGridEventPublisher(EventGridPublisherClient eventGridClient, EventGridEventPublisherSettings settings = null)
+    /// <inheritdoc cref="IEventGridEventPublisher"/>
+    public async Task PublishEventsAsync(string sendGridEventsJson, CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrEmpty(sendGridEventsJson))
+            throw new ArgumentException("Cannot be null, empty or whitespace.", nameof(sendGridEventsJson));
+
+        var events = JsonConvert.DeserializeObject<IEnumerable<JObject>>(sendGridEventsJson).ToArray();
+        if (!events.Any()) return;
+
+        var eventGridEvents = events
+            .Select(ToEventGridEvent)
+            .ToArray();
+
+        await _eventGridClient.SendEventsAsync(eventGridEvents, cancellationToken: cancellationToken);
+    }
+
+    private const string EventIdKey = "sg_event_id";
+    private EventGridEvent ToEventGridEvent(JObject eventJson)
+    {
+        return new EventGridEvent(
+            _settings.BuildEventSubject(eventJson),
+            _settings.BuildEventType(eventJson),
+            "1", 
+            new BinaryData(Encoding.UTF8.GetBytes(eventJson.ToString())))
         {
-            _eventGridClient = eventGridClient ?? throw new ArgumentNullException(nameof(eventGridClient));
-            _settings = settings ?? EventGridEventPublisherSettings.Default;
-        }
-
-        /// <inheritdoc cref="IEventGridEventPublisher"/>
-        public async Task PublishEventsAsync(string sendGridEventsJson, CancellationToken cancellationToken = default)
-        {
-            if (string.IsNullOrEmpty(sendGridEventsJson))
-                throw new ArgumentException("Cannot be null, empty or whitespace.", nameof(sendGridEventsJson));
-
-            var events = JsonConvert.DeserializeObject<IEnumerable<JObject>>(sendGridEventsJson).ToArray();
-            if (!events.Any()) return;
-
-            var eventGridEvents = events
-                .Select(ToEventGridEvent)
-                .ToArray();
-
-            await _eventGridClient.SendEventsAsync(eventGridEvents, cancellationToken: cancellationToken);
-        }
-
-        private const string EventIdKey = "sg_event_id";
-        private EventGridEvent ToEventGridEvent(JObject eventJson)
-        {
-            return new EventGridEvent(
-                _settings.BuildEventSubject(eventJson),
-                _settings.BuildEventType(eventJson),
-                "1", 
-                new BinaryData(Encoding.UTF8.GetBytes(eventJson.ToString())))
-            {
-                Id = eventJson.TryGetValue(EventIdKey, StringComparison.OrdinalIgnoreCase, out var token)
-                    ? token.Value<string>()
-                    : Guid.NewGuid().ToString()
-            };
-        }
+            Id = eventJson.TryGetValue(EventIdKey, StringComparison.OrdinalIgnoreCase, out var token)
+                ? token.Value<string>()
+                : Guid.NewGuid().ToString()
+        };
     }
 }
--- src/SendGrid.Azure.EventGrid/IEventGridEventPublisher.cs
+++ src/SendGrid.Azure.EventGrid/IEventGridEventPublisher.cs
@@ -1,19 +1,18 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 
-namespace Moosesoft.SendGrid.Azure.EventGrid
+namespace Moosesoft.SendGrid.Azure.EventGrid;
+
+/// <summary>
+/// Defines an event publisher that reads raw json SendGrid events and publishes them as Azure EventGrid events.
+/// </summary>
+public interface IEventGridEventPublisher
 {
     /// <summary>
-    /// Defines an event publisher that reads raw json SendGrid events and publishes them as Azure EventGrid events.
+    /// Reads raw json SendGrid events and publishes them as Azure EventGrid events to a specified topic endpoint.
     /// </summary>
-    public interface IEventGridEventPublisher
-    {
-        /// <summary>
-        /// Reads raw json SendGrid events and publishes them as Azure EventGrid events to a specified topic endpoint.
-        /// </summary>
-        /// <param name="sendGridEventsJson">Raw json events received from SendGrid.</param>
-        /// <param name="cancellationToken">Cancellation token used to cancel the operation.</param>
-        /// <returns>The <see cref="Task"/> returned which can be awaited.</returns>
-        Task PublishEventsAsync(string sendGridEventsJson, CancellationToken cancellationToken = default);
-    }
+    /// <param name="sendGridEventsJson">Raw json events received from SendGrid.</param>
+    /// <param name="cancellationToken">Cancellation token used to cancel the operation.</param>
+    /// <returns>The <see cref="Task"/> returned which can be awaited.</returns>
+    Task PublishEventsAsync(string sendGridEventsJson, CancellationToken cancellationToken = default);
 }
